8c6a9cabe8f84eb6407530b6cc10d4235f3bbe02
Sagie Bar-Hame <2603sagie@gmail.com>
2020-10-22 21:48:39 +0300

    first commit

---
+++ TaskManager/Controllers/TasksController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+using TaskManager.Services;
+
+namespace TaskManagerAsp.Net.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly IRepository<TasksManager> _taskRepositoryService;
+
+        public TasksController(IRepository<TasksManager> taskRepositoryService)
+        {
+            this._taskRepositoryService = taskRepositoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(_taskRepositoryService.GetById(id));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddNewTask(TasksManager task)
+        {
+            if (ModelState.IsValid)
+            {
+                bool added = _taskRepositoryService.Create(task);
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                bool deleted = _taskRepositoryService.Delete(id);
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Update(TasksManager task)
+        {
+            _taskRepositoryService.Update(task);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(TasksManager task)
+        {
+            return View(task);
+        }
+    }
+}
---
+++ TaskManager/Models/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class TasksManager
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool Done { get; set; }
+        public string Title { get; set; }
+        public int Priority { get; set; }
+    }
+}
